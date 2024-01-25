5e93bd42acc737d08cff375084b59ae9bb7a0192
cbijeaux <115676825+cbijeaux@users.noreply.github.com>
2024-01-24 19:39:50 -0500

    Update StableMatching.py
    adjusted pathingcreator to be able to find the files in the folder.

--- StableMatching.py
+++ StableMatching.py
@@ -8,7 +8,7 @@ def pathing_creator(name:str)->str:
         path=os.path.dirname(os.path.realpath(sys.executable))  
     elif __file__:
         path=os.path.dirname(__file__)
-    return os.path.join(path,name)
+    return os.path.join(path,'MatchingInput',name)
 
 file=pathing_creator('input.txt')
 applicantdata=[]
